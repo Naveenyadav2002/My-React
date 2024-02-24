92b81cfeb0278e376d6bbc95d804b5461edb9622
Naveenyadav2002 <naveenyadav1622@gmail.com>
2024-02-24 16:18:08 +0530

    react started

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 
 
 
-import ImageComponent from './component/image/image-component';
-import TextComponent from './component/text/text';
-import ButtonComponent from './component/button/button-component';
-import OrderList from './component/list-component/order-list';
-import UnorderList from './component/list-component/unorder-list';
+// import ImageComponent from './component/image/image-component';
+// import TextComponent from './component/text/text';
+// import ButtonComponent from './component/button/button-component';
+// import OrderList from './component/list-component/order-list';
+// import UnorderList from './component/list-component/unorder-list';
 
 
 function App() {
   return (
     <div>
 
-    <h2 className="na">Hello...WORLD</h2>
-    <TextComponent/>
-    <ImageComponent/><br></br>
-    {/* <h2>Hello</h2> */}
-    <h3>Today's components</h3>
-    <b>Orderlist</b>
-    <OrderList/>
-    <b>Unorderlist</b>
-    <UnorderList/>
-    <ButtonComponent/>
+    <h2 className="na">Hello...WELCOME TO REACT</h2>
+
+
+    
+
     </div>
     
    
